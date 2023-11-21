9cf288637a5a15879e0829a0a3838addb8dbda04
RenildoJr <juninho952020@gmail.com>
2023-11-21 18:35:48 -0300

    searchbox quase pronta

--- website/themes/Gradiante/js/main.js
+++ website/themes/Gradiante/js/main.js
@@ -1,16 +1,32 @@
-const searchform = document.querySelector(".searchform")
-const searchbar = document.getElementById("searchbar");
-// searchbar.style.width = "150px"
+'use strict';
+// Searchbox
+const headerSearchForm = document.querySelector(".page-header .searchform");
+const headerSearchbox = headerSearchForm.querySelector(".searchbox");
+const headerSearchbar = headerSearchbox.querySelector("#searchbar");
+const btnSearchClose = headerSearchbox.querySelector("#btn-close_searchbar");
+const btnSearchOpen = headerSearchForm.querySelector("#btn-open_search");
+const btnSearchSubmit = headerSearchForm.querySelector("#btn-submit_search");
 
-const btnSearch = document.getElementById("btn-submit_search");
+function searchboxOpen() {
+    headerSearchbox.classList.add("searchbox--active");
+    btnSearchClose.addEventListener("click", searchboxClose);
+    btnSearchOpen.style.display = "none";
+    btnSearchSubmit.style.display = "block"
 
-searchform.addEventListener("submit", e => {
-    e.preventDefault();
-})
+    btnSearchSubmit.addEventListener("click", () => {headerSearchForm.submit()});
+    headerSearchbar.addEventListener("keypress", () => {
+        if (event.key === "Enter") {
+            headerSearchForm.submit();
+        }
+    });
+}
 
-btnSearch.addEventListener("click", () => {
-    // searchbar.classList.add("searchbar--active");
-    searchbar.style.width = "150px";
-    searchbar.style.display = "flex";
+function searchboxClose() {
+    headerSearchbox.classList.remove("searchbox--active");
+    btnSearchOpen.style.display = "block";
+    btnSearchSubmit.style.display = "none";
+}
 
-})
+headerSearchForm.addEventListener("submit", e => {e.preventDefault()});
+btnSearchOpen.addEventListener("click", searchboxOpen);
+btnSearchClose.addEventListener("click", searchboxClose);
